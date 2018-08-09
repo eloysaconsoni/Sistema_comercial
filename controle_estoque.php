78a5216753b6f1ed5d06c28bc8cea16d690f6c4d
Passareli <38391699+Passareli@users.noreply.github.com>
2018-08-08 21:56:34 -0300

    UPDATE
    Arrumado os Nomes dos Paineis

--- controle_estoque.php
+++ controle_estoque.php
@@ -1,7 +1,8 @@
 <!-- quando a barra for digitada ele ira localizar o item em imprime.php -->
 
 <form method="post" action="imprime.php">
+	<p>Controle de Estoque</p>
 	<p>Para pesquisar um produto digite o código de barras</p>
 	<p><label for="barra_dig"> Barras:</label><input id="barra_dig" type="text" name="cod_barras"></p>
 </form>
-       <a href="menu.php"><button>Cancelar</button></a>
+       <a href="menu.php"><button>Voltar</button></a>
--- entrada.php
+++ entrada.php
@@ -9,13 +9,15 @@
 <body>
 	<form method="post" action="controle.php" oninput="calc_total();">
 		<fieldset>
+			<p>Entrada de Produtos</p>
 			<p><label for="dataen">Data da entrada:</label><input id="dataen" type="date" name="data"></p>
 			<p><label for="barraen"> Barras:</label><input id="barraen" type="text" name="cod_barras"></p>
 			<label for="descen">Descrição:</label><textarea id="descen" required="required"></textarea> 
 			<p><label for="qtdeen"> Quantidade:</label> <input id="qtdeen" type="number" name="Cqtde"required="required"></p>
 			<p><label for="val_unien"> Valor Unitário: R$</label><input id="val_unien" type="number" name="val_unit"></p>
 			<p><label for="val_toten">Valor Total: R$ </label> <input id="val_toten" type="text" name="val_total" readonly=""></p>
-			<p><input type="submit" name="gravar" value="Gravar"></p>
+			
 		</fieldset>
 	</form>
-			<a href="menu.php"><button>Cancelar</button></a>
+			<p><input type="submit" name="gravar" value="Gravar"></p>
+			<a href="menu.php"><button>Voltar</button></a>
